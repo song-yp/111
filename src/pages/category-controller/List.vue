0702edea30f1ab276ac999749ad6e4d7dfdcfe41
song-yp <1043591966@qq.com>
2020-01-03 16:31:24 +0800

    顾客管理，员工管理

---
+++ src/pages/category-controller/List.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <el-button type="success" size="small" @click="toAddHandler">添加</el-button> 
+        <el-button type="danger" size="small">批量删除</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
